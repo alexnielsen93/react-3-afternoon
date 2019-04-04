d2b169c1e9fa0de74cabb202c29dee3fc9fac38f
Alex Nielsen <alexnielsen@Alexs-MacBook-Pro.local>
2019-04-04 15:17:59 -0600

    donnnnnnnnne

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 import './App.css';
-
+import axios from 'axios'
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
+// import { ToastContainer, toast } from 'react-toastify';
+// BASE URL:      practiceapi.devmountain.com/api
 
 class App extends Component {
   constructor() {
@@ -19,19 +22,59 @@ class App extends Component {
   }
   
   componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts').then((res)=>{
+      // console.log(res)
+      let {data} = res
+      this.setState({
+        posts: data
+      })
+      
+    })
+    .catch((err)=>{
+      console.log(err, "error")
+    })
+  }
+
+  updatePost(id, text) {
+  axios.put(`https://practiceapi.devmountain.com/api/posts/?id=${id}`, {text: text}).then((res)=>{
+    console.log(res)
+    let {data} = res
+    this.setState({
+      posts: data
+    })
+  })
+  .catch((err)=>{
+    console.log(err,"error")
+  })
+
 
   }
 
-  updatePost() {
-  
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts/?id=${id}`).then((res)=>{
+      console.log(res)
+      let {data} = res
+      this.setState({
+        posts: data
+      })
+    })
+    .catch((err)=>{
+      console.log(err, 'error')
+    })
   }
 
-  deletePost() {
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts/', {text:text}).then((res)=>{
+      console.log(res)
+      let {data } = res
+      this.setState({
+      posts: data
+      })
+    })
+    .catch((err) =>{
+      console.log(err, 'create post error')
 
-  }
-
-  createPost() {
-
+    })
   }
 
   render() {
@@ -43,8 +86,11 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose  createPostFn = {this.createPost}/>
+          {posts.map((post)=>{
+            return <Post key = {post.id} text = {post.text} date = {post.date} updatePostFn ={this.updatePost} id ={post.id} deletePostFn= {this.deletePost}/>
+          })}
+
         </section>
       </div>
     );
